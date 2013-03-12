b7ea828cdff7b6766df4c4c459bbabf60ee99c26
Pierre Chalamet <pierre@chalamet.net>
2013-03-12 22:41:06 +0100

    using cassandra-sharp 3.1

--- cassandra-sharp-contrib-version.cs
+++ cassandra-sharp-contrib-version.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.0.1")]
-[assembly: AssemblyFileVersion("3.0.1")]
-[assembly: AssemblyInformationalVersion("3.0.1")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AssemblyInformationalVersion("3.1.0")]
 
 
